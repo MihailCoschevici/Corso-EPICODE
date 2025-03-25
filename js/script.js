eade0fd7f173e5524fd74a1c06dec03979e1d1d1
MihailCoschevici <coschevici_mihai@yahoo.com>
2025-03-25 01:42:50 +0100

    ho aggunto altri articoli e la sezione twitter

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let zucchero
+zucchero = 100
+console.log('Il valaore iniziale e'+ zucchero)
+let farina = 75
+zucchero = 125.5
+console.log('Il valore iniziale e ' +zucchero)
+zucchero = 100
+console.log('Il valore inizai')
