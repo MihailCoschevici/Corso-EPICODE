65e37ab39e3c5109f624bd5a5ae08cfea837c0b1
Mihail Coschevici <coschevici_mihai@yahoo.com>
2025-03-25 19:29:21 +0100

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-let zucchero
-zucchero = 100
-console.log('Il valaore iniziale e'+ zucchero)
-let farina = 75
-zucchero = 125.5
-console.log('Il valore iniziale e ' +zucchero)
-zucchero = 100
-console.log('Il valore inizai')
