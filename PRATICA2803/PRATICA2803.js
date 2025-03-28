34ad42dd3d052c110f8db426dcba87438bcb927e
MihailCoschevici <coschevici_mihai@yahoo.com>
2025-03-28 18:38:07 +0100

    ESERCIZIO PRATICCO 2803 JAVASCRIPT

---
+++ PRATICA2803/PRATICA2803.js
@@ -0,0 +1,67 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+
+/* 
+ Nell mondo di Javascript ci sono diversi tipi di dati, che possiamo metteere diverse cose dentro a una scatola.
+ - Number (come 4,5,6,7,8,9) come la eta di una persona, o con le virgole (4.5,6.8,7.9) come il prezzo di un prodotto.
+ -String (come buongiorno,ciao come stai,ecc) che sono le parole che gli usiamo per comunicare.
+ -Boolean (true o false) sono accesione di un dispositivo,come una pc che e acceso o spento
+- Null e come una scatola vuota, che contiene anche adessivo scrito niente sopra.
+-Undefined e come una scatola che non sappiamo cosa po contenere,e non sappiamo se e vuota o piena.
+-Object e come una scatola che contiene tante altri scatole al interno ognuno con un nome diverso del altra.*/
+
+/* ESERCIZIO 2
+ Descrivi cos'è un oggetto in JavaScript, con parole tue.
+*/
+
+/* Un oggetto  e come  una scatola piena di diverse altre scatole che al interno per esempio ci sono carte de identita che contiene tutte le informazioni importanti su qualcosa o qualcuno.Ogni informazione ha un suo nome  e un valore associato. */
+
+/* ESERCIZIO 3
+ Scriti il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+/* let somma = 12+10
+console.log(somma); //Stampa 22 */
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/* let x = 12;*/
+
+/* ESERCIZIO 5
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+/* let name = "Mihail";  */
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+/* let x = 12;
+let risultato =4 -x;
+console.log(risultato) ; //stampa -8
+*/
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ Infine, verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+ NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
+*/
+
+/* RISPOSTA NEL FILE ESTERNO PRATICA2803/ESSERCIZIO7.JS
+
+ */
