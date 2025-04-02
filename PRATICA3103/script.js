2a1ece6b841400f5bc747299a4ff2ef66a6a4010
MihailCoschevici <coschevici_mihai@yahoo.com>
2025-04-02 11:23:37 +0200

    ESSERCIZIO PRATICA3103

---
+++ PRATICA3103/script.js
@@ -0,0 +1,62 @@
+   /* let num1 = 10;
+    let num2 = 25;
+    if(num1 > num2) { 
+    console.log("Il numere piu grande " + num1);
+    } else if (num2 > num1) 
+    console.log("Il numero più grande è: " + num2);
+ else {
+    console.log("I numeri sono uguali.");
+}*/
+
+
+/* 
+for (let num = 0; num <= 10; num++) {
+    if (num === 3 || num === 8) {
+        continue;
+    }
+    console.log(num);
+}*/
+
+/*ESERCIZIO 2
+
+let num = 20;
+
+if (num < 5) {
+    console.log("Tiny");
+} else if (num < 10) {
+    console.log("Small");
+} else if (num < 15) {
+    console.log("Medium");
+} else if (num < 20) {
+    console.log("Large");
+} else {
+    console.log("Huge");
+}*/
+
+
+/*ESERCIZIO 11
+for (let i = 0; i <= 15; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " è pari");
+    } else {
+        console.log(i + " è dispari");
+    }
+}*/
+
+/*ESERCIZIO EXTRA 1
+let num1 = 10;
+  let num2 = 2;
+if (num1 === 8 || num2 ===8 ){
+  console.log("Uno dei numeri è 8.");
+} else if (num1 + num2 === 8) {
+  console.log("La loro somma è 8.");
+} else if (Math.abs(num1 - num2) === 8) {
+  console.log("La loro differenza è 8.");
+} else {
+  console.log("Nessuna delle condizioni è soddisfatta.");
+}*/
+
+/*ESERCIZIO EXTRA 4
+let isMale = true; 
+let gender = isMale ? "male" : "female";
+console.log("Gender:", gender);*/
